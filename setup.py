e037251e235b17934fce552a497e86e6930ab246
gaurabacharya <acharyaagaurab@gmail.com>
2025-04-02 13:23:53 -0700

    New project structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical Rag Chatbot",
+    version = "0.0.0",
+    author = "Gaurab Acharya",
+    author_email = "gaurab_acharya@outlook.com",
+    packages = find_packages(),
+    install_requires = []
+)
+
